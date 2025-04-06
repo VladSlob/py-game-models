01839a4517e8c7db4484edce3bc409b838d922c1
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-06 20:19:51 +0300

    models.py

--- db/models.py
+++ db/models.py
@@ -1,28 +1,22 @@
 from django.db import models
 
-from django.db.models import SET_NULL
-
-
 class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
-
 class Skill(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, null=True, on_delete=SET_NULL)
-
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='skills')
 
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
 
-
 class Player(models.Model):
-    nickname = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=SET_NULL, null=True)
-    guild = models.ForeignKey(Guild, on_delete=SET_NULL, null=True)
-    created_at = models.DateTimeField(auto_now=True)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='players')
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True, related_name='members')
+    created_at = models.DateTimeField(auto_now_add=True)
